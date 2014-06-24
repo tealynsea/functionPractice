a3be9085c671f89292c77918012fe26146024830
Teri <tealynsea@hotmail.com>
2014-06-24 12:49:41 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+var tripleFive = function() {
+	
+	for(var i=0; i<3; i++) {
+	console.log("Five !");
+	}
+}
+
+tripleFive();
+
+var lastLetter = function(string) {
+ 
+	return string[string.length-1]
+
+}
+lastLetter('island')
+
+var square = function (n) {
+
+	return n * n;
+
+}
+
+square(5);
+
+var negate = function(n) {
+
+	return n * -1;
+}
+
+negate(5);
