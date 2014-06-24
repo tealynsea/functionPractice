7cd4c60a36d8908011387fac8c08a9861e96912a
Teri <tealynsea@hotmail.com>
2014-06-24 17:01:48 -0600

    Exercise completed

--- main.js
+++ main.js
@@ -28,3 +28,70 @@ var negate = function(n) {
 }
 
 negate(5);
+
+
+var toArray = function(x, y, z){ 
+return [x, y, z];
+
+}
+
+toArray(1, 4, 5)
+
+var startsWithA = function(word) {
+	 
+	 return word.charAt(0).toUpperCase() === 'A';
+
+  }
+ 
+
+startsWithA('bear')
+
+
+var excite = function(word) {
+	 
+	return word + "!!!";
+}
+
+console.log(excite('yes'));
+
+var sun = function(word) { 
+
+	return word.indexOf("sun") != -1;
+	
+}
+
+console.log(sun('block'));
+
+var tiny = function(number) {
+
+	if(number <=1 && number>=0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+console.log(tiny(4));
+
+var getSeconds = function(time)  {
+
+	var pieces = time.split(":");
+	var result = Number(pieces[0]) * 60 + Number(pieces[1])
+	return(result);
+}
+
+console.log(getSeconds("10:30"))
+
+	 
+
+
+
+
+
+
+
+
+
+
+
